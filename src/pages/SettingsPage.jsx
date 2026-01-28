3c6479a37fc1efbb56788dca30ba527373a630ba
lancherix <lancherix.service@gmail.com>
2026-01-27 21:21:22 -0500

    k iara

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -811,7 +811,9 @@ const SettingsPage = () => {
             </div>
           </div>
           <div className='options-settingsPage'>
-            <button onClick={() => setSelectedOption('My profile')}><img src={UserIcon} className='resultIcon-coolPage' alt="Edit my profile" />Edit my Profile</button>
+            <button onClick={() => setSelectedOption('My profile')}>{/*<img src={UserIcon} className='resultIcon-coolPage' alt="Edit my profile" />*/}<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+              <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clip-rule="evenodd" />
+            </svg>Edit my Profile</button>
             <button onClick={() => setSelectedOption('Aspect')}><img src={AspectIcon} className='resultIcon-coolPage' alt="Aspect" />Aspect</button>
             <button onClick={() => setSelectedOption('Terms of use')}><img src={TermsOfUseIcon} className='resultIcon-coolPage' alt="Terms of use" />Terms of Use</button>
             <button onClick={() => setShowLogoutConfirmation(true)}><img src={LogoutIcon} className='resultIcon-coolPage' alt="Logout" />Logout</button>
