5606e883abe6b4f0067b92691595fbd61f96d891
lancherix <lancherix.service@gmail.com>
2026-02-01 17:07:29 -0500

    ki ara

--- src/pages/AllProjectsPage.jsx
+++ src/pages/AllProjectsPage.jsx
@@ -3,72 +3,73 @@ import { Link } from 'react-router-dom';
 import './Styles/AllProjectsPage.css';
 
 const AllProjectsPage = () => {
-  return (
-    <div className="all-projectsPage">
-      <div className="window-projectsPage">
+    return (
+        <div className="all-projectsPage">
+            <div className="window-projectsPage">
 
-        {/* ===== Header ===== */}
-        <div className="header-projectsPage">
-          {/* Left */}
-          <div className="headerLeft-projectsPage">
-            <button className="addProjectBtn-projectsPage">＋</button>
-          </div>
+                {/* ===== Header ===== */}
+                <div className="header-projectsPage">
+                    {/* Left */}
+                    <div className="headerLeft-projectsPage">
+                        <button className="addProjectBtn-projectsPage">＋</button>
+                    </div>
 
-          {/* Columns */}
-          <div className="headerCols-projectsPage projectsGrid">
-            <div className="col-name-projectsPage">Name</div>
-            <div className="col-status-projectsPage">Status</div>
-            <div className="col-priority-projectsPage">Priority</div>
-            <div className="col-members-projectsPage">Members</div>
-            <div className="col-visibility-projectsPage">Visibility</div>
-            <div className="col-deadline-projectsPage">Deadline</div>
-            <div className="col-updated-projectsPage">Updated</div>
-            <div className="col-actions-projectsPage" />
-          </div>
+                    {/* Columns */}
+                    <div className="headerCols-projectsPage projectsGrid">
+                        <div className="col-name-projectsPage">Name</div>
+                        <div className="col-status-projectsPage">Status</div>
+                        <div className="col-priority-projectsPage">Priority</div>
+                        <div className="col-members-projectsPage">Members</div>
+                        <div className="col-visibility-projectsPage">Visibility</div>
+                        <div className="col-deadline-projectsPage">Deadline</div>
+                        <div className="col-updated-projectsPage">Updated</div>
+                        <div className="col-actions-projectsPage" />
+                    </div>
 
-          {/* Right */}
-          <div className="headerRight-projectsPage">
-            <input
-              type="text"
-              className="search-projectsPage"
-              placeholder="Search"
-            />
-          </div>
+                    {/* Right */}
+                    <div className="headerRight-projectsPage">
+                        <input
+                            type="text"
+                            className="search-projectsPage"
+                            placeholder="Search"
+                        />
+                    </div>
+                </div>
+
+                {/* ===== Content ===== */}
+                <div className="content-projectsPage">
+                    {/* ===== Header ===== */}
+                    <div className="header-projectsPage">
+                        {/* Left */}
+                        <div className="headerLeft-projectsPage">
+                            <button className="addProjectBtn-projectsPage">＋</button>
+                        </div>
+
+                        {/* Columns */}
+                        <div className="headerCols-projectsPage projectsGrid">
+                            <div className="col-name-projectsPage">Name</div>
+                            <div className="col-status-projectsPage">Status</div>
+                            <div className="col-priority-projectsPage">Priority</div>
+                            <div className="col-members-projectsPage">Members</div>
+                            <div className="col-visibility-projectsPage">Visibility</div>
+                            <div className="col-deadline-projectsPage">Deadline</div>
+                            <div className="col-updated-projectsPage">Updated</div>
+                            <div className="col-actions-projectsPage" />
+                        </div>
+
+                        {/* Right */}
+                        <div className="headerRight-projectsPage">
+                            <input
+                                type="text"
+                                className="search-projectsPage"
+                                placeholder="Search"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        {/* ===== Content ===== */}
-        <div className="content-projectsPage">
-
-          {/* Left */}
-          <div className="headerLeft-projectsPage">
-            <button className="addProjectBtn-projectsPage">＋</button>
-          </div>
-
-          {/* Columns */}
-          <div className="headerCols-projectsPage projectsGrid">
-            <div className="col-name-projectsPage">Name</div>
-            <div className="col-status-projectsPage">Status</div>
-            <div className="col-priority-projectsPage">Priority</div>
-            <div className="col-members-projectsPage">Members</div>
-            <div className="col-visibility-projectsPage">Visibility</div>
-            <div className="col-deadline-projectsPage">Deadline</div>
-            <div className="col-updated-projectsPage">Updated</div>
-            <div className="col-actions-projectsPage" />
-          </div>
-
-          {/* Right */}
-          <div className="headerRight-projectsPage">
-            <input
-              type="text"
-              className="search-projectsPage"
-              placeholder="Search"
-            />
-          </div>
-
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default AllProjectsPage;
