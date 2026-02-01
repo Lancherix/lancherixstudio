1c9f943614f856a76a587b62f2b6091b1dc8599f
lancherix <lancherix.service@gmail.com>
2026-02-01 17:12:54 -0500

    ki ara

--- src/pages/AllProjectsPage.jsx
+++ src/pages/AllProjectsPage.jsx
@@ -42,18 +42,45 @@ const AllProjectsPage = () => {
                     <div className="header-projectsPage">
                         {/* Left */}
                         <div className="headerLeft-projectsPage">
-                            <button className="addProjectBtn-projectsPage">＋</button>
+                            <button className="addProjectBtn-projectsPage">🤍</button>
                         </div>
 
                         {/* Columns */}
                         <div className="headerCols-projectsPage projectsGrid">
-                            <div className="col-name-projectsPage">Name</div>
-                            <div className="col-status-projectsPage">Status</div>
-                            <div className="col-priority-projectsPage">Priority</div>
-                            <div className="col-members-projectsPage">Members</div>
-                            <div className="col-visibility-projectsPage">Visibility</div>
-                            <div className="col-deadline-projectsPage">Deadline</div>
-                            <div className="col-updated-projectsPage">Updated</div>
+                            <div className="col-name-projectsPage">Kiarita</div>
+                            <div className="col-status-projectsPage">📌</div>
+                            <div className="col-priority-projectsPage">Low</div>
+                            <div className="col-members-projectsPage">2</div>
+                            <div className="col-visibility-projectsPage">Private</div>
+                            <div className="col-deadline-projectsPage">-</div>
+                            <div className="col-updated-projectsPage">-</div>
+                            <div className="col-actions-projectsPage" />
+                        </div>
+
+                        {/* Right */}
+                        <div className="headerRight-projectsPage">
+                            <input
+                                type="text"
+                                className="search-projectsPage"
+                                placeholder="Search"
+                            />
+                        </div>
+                    </div>
+                    <div className="header-projectsPage">
+                        {/* Left */}
+                        <div className="headerLeft-projectsPage">
+                            <button className="addProjectBtn-projectsPage">🌊</button>
+                        </div>
+
+                        {/* Columns */}
+                        <div className="headerCols-projectsPage projectsGrid">
+                            <div className="col-name-projectsPage">Physics</div>
+                            <div className="col-status-projectsPage">Active</div>
+                            <div className="col-priority-projectsPage">High</div>
+                            <div className="col-members-projectsPage">1</div>
+                            <div className="col-visibility-projectsPage">Public</div>
+                            <div className="col-deadline-projectsPage">-</div>
+                            <div className="col-updated-projectsPage">-</div>
                             <div className="col-actions-projectsPage" />
                         </div>
 
