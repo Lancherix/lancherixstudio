e6a1bfaff30571d8dab3e852b51ee2ddbcc979fb
lancherix <lancherix.service@gmail.com>
2026-05-19 20:35:50 -0500

    Arkansas

--- src/pages/Mobile/ProjectPageMobile.jsx
+++ src/pages/Mobile/ProjectPageMobile.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
-import "./Styles/ProjectPageMobile.css";
+import "./ProjectPageMobile.css";
 
 const ProjectPageMobile = () => {
   const { slug } = useParams();
