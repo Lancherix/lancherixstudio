49e6a27975c650c88373262fd0856987cc819b27
lancherix <lancherix.service@gmail.com>
2026-08-28 14:35:45 -0500

    Garizmendi

--- src/i18n.js
+++ src/i18n.js
@@ -3,7 +3,9 @@ import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 import enUS from './locales/en-US.json';
+import esCO from './locales/es-CO.json';
 import frFR from './locales/fr-FR.json';
+import ruRU from './locales/ru-RU.json';
 
 i18n
   .use(LanguageDetector)
@@ -11,10 +13,13 @@ i18n
   .init({
     resources: {
       'en-US': { translation: enUS },
+      'es-CO': { translation: esCO },
       'fr-FR': { translation: frFR },
+      'ru-RU': { translation: ruRU },
     },
     lng: 'en-US',
     fallbackLng: 'en-US',
+    supportedLngs: ['en-US', 'es-CO', 'fr-FR', 'ru-RU'],
     interpolation: { escapeValue: false },
   });
 
